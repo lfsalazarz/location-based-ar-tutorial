7314b8d6e3bd66d69a98862e006c6ded35624b2e
Desarrollo 2 <desarrollo2@MacBook-Pro-de-Desarrollo.local>
2020-01-29 18:36:22 -0500

    renderPlaces

--- script.js
+++ script.js
@@ -119,17 +119,131 @@
 
 
 
+// // // window.onload = () => {
+// // //     let method = 'dynamic';
+
+// // //     // if you want to statically add places, de-comment following line:
+// // //     method = 'static';
+// // //     if (method === 'static') {
+// // //         let places = staticLoadPlaces();
+// // //         return renderPlaces(places);
+// // //     }
+
+// // //     if (method !== 'static') {
+// // //         // first get current user location
+// // //         return navigator.geolocation.getCurrentPosition(function (position) {
+
+// // //             // than use it to load from remote APIs some places nearby
+// // //             dynamicLoadPlaces(position.coords)
+// // //                 .then((places) => {
+// // //                     renderPlaces(places);
+// // //                 })
+// // //         },
+// // //             (err) => console.error('Error in retrieving position', err),
+// // //             {
+// // //                 enableHighAccuracy: true,
+// // //                 maximumAge: 0,
+// // //                 timeout: 27000,
+// // //             }
+// // //         );
+// // //     }
+// // // };
+
+// // // function staticLoadPlaces() {
+// // //     return [
+// // //         {
+// // //             name: 'Place1',
+// // //             location: {
+// // //                 lat: 6.235899925231934,
+// // //                 lng: -75.57510375976562,
+// // //             }
+// // //         },
+// // //         {
+// // //             name: 'Place2',
+// // //             location: {
+// // //                 lat: 6.200326,
+// // //                 lng: -75.573206,
+// // //             }
+// // //         },
+// // //         {
+// // //             name: 'Place3',
+// // //             location: {
+// // //                 lat: 6.199532,
+// // //                 lng: -75.572384,
+// // //             }
+// // //         },
+
+// // //     ];
+// // // }
+
+// // // // getting places from REST APIs
+// // // function dynamicLoadPlaces(position) {
+// // //     let params = {
+// // //         radius: 300,    // search places not farther than this value (in meters)
+// // //         clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
+// // //         clientSecret: '',
+// // //         version: '20300101',    // foursquare versioning, required but unuseful for this demo
+// // //     };
+
+// // //     // CORS Proxy to avoid CORS problems
+// // //     let corsProxy = 'https://cors-anywhere.herokuapp.com/';
+
+// // //     // Foursquare API
+// // //     let endpoint = `${corsProxy}https://api.foursquare.com/v2/venues/search?intent=checkin
+// // //         &ll=${position.latitude},${position.longitude}
+// // //         &radius=${params.radius}
+// // //         &client_id=${params.clientId}
+// // //         &client_secret=${params.clientSecret}
+// // //         &limit=15
+// // //         &v=${params.version}`;
+// // //     return fetch(endpoint)
+// // //         .then((res) => {
+// // //             return res.json()
+// // //                 .then((resp) => {
+// // //                     return resp.response.venues;
+// // //                 })
+// // //         })
+// // //         .catch((err) => {
+// // //             console.error('Error with places API', err);
+// // //         })
+// // // };
+
+// // // function renderPlaces(places) {
+// // //     let scene = document.querySelector('a-scene');
+
+// // //     places.forEach((place) => {
+// // //         let latitude = place.location.lat;
+// // //         let longitude = place.location.lng;
+
+// // //         // add place name
+// // //         // let text = document.createElement('a-link');
+// // //         // text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+// // //         // text.setAttribute('title', place.name);
+// // //         // text.setAttribute('href', 'http://www.example.com/');
+// // //         // text.setAttribute('scale', '15 15 15');
+
+// // //         text.addEventListener('loaded', () => {
+// // //             window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
+// // //         });
+
+// // //         scene.appendChild(text);
+// // //     });
+// // // }
+
+
 window.onload = () => {
     let method = 'dynamic';
 
-    // if you want to statically add places, de-comment following line:
+    // if you want to statically add places, de-comment following line
     method = 'static';
+
     if (method === 'static') {
         let places = staticLoadPlaces();
-        return renderPlaces(places);
+        renderPlaces(places);
     }
 
     if (method !== 'static') {
+
         // first get current user location
         return navigator.geolocation.getCurrentPosition(function (position) {
 
@@ -172,7 +286,6 @@ function staticLoadPlaces() {
                 lng: -75.572384,
             }
         },
-
     ];
 }
 
@@ -180,8 +293,8 @@ function staticLoadPlaces() {
 function dynamicLoadPlaces(position) {
     let params = {
         radius: 300,    // search places not farther than this value (in meters)
-        clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',
-        clientSecret: '',
+        clientId: 'HZIJGI4COHQ4AI45QXKCDFJWFJ1SFHYDFCCWKPIJDWHLVQVZ',   // add your credentials here
+        clientSecret: '',   // add your credentials here
         version: '20300101',    // foursquare versioning, required but unuseful for this demo
     };
 
@@ -212,20 +325,44 @@ function renderPlaces(places) {
     let scene = document.querySelector('a-scene');
 
     places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
+        const latitude = place.location.lat;
+        const longitude = place.location.lng;
 
-        // add place name
-        let text = document.createElement('a-link');
-        text.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-        text.setAttribute('title', place.name);
-        text.setAttribute('href', 'http://www.example.com/');
-        text.setAttribute('scale', '15 15 15');
+        // add place icon
+        const icon = document.createElement('a-image');
+        icon.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude}`);
+        icon.setAttribute('name', place.name);
+        icon.setAttribute('src', './assets/logo-bluspot.png');
 
-        text.addEventListener('loaded', () => {
-            window.dispatchEvent(new CustomEvent('gps-entity-place-loaded'))
-        });
+        // for debug purposes, just show in a bigger scale, otherwise I have to personally go on places...
+        icon.setAttribute('scale', '20, 20');
 
-        scene.appendChild(text);
+        icon.addEventListener('loaded', () => window.dispatchEvent(new CustomEvent('gps-entity-place-loaded')));
+
+        const clickListener = function (ev) {
+            ev.stopPropagation();
+            ev.preventDefault();
+
+            const name = ev.target.getAttribute('name');
+
+            const el = ev.detail.intersection && ev.detail.intersection.object.el;
+
+            if (el && el === ev.target) {
+                const label = document.createElement('span');
+                const container = document.createElement('div');
+                container.setAttribute('id', 'place-label');
+                label.innerText = name;
+                container.appendChild(label);
+                document.body.appendChild(container);
+
+                setTimeout(() => {
+                    container.parentElement.removeChild(container);
+                }, 1500);
+            }
+        };
+
+        icon.addEventListener('click', clickListener);
+
+        scene.appendChild(icon);
     });
 }
